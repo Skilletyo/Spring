5a3f5a05a94a029233bca226ec2b1f3440f5154d
Troglodyte <88345450+Skilletyo@users.noreply.github.com>
2024-04-19 22:03:20 +0200

    Cleaned up the character controller code, added a little secret.

---
+++ Scripts/Stereo.gd
@@ -0,0 +1,20 @@
+extends RigidBody3D
+
+signal interacted_with
+
+@onready var musicPlayer = $AudioStreamPlayer3D
+var isPlaying = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	pass
+	
+func _on_interacted_with():
+	if musicPlayer.playing:
+		musicPlayer.stop()
+	else:
+		musicPlayer.play()
+	
