476295bf944685db667f7bb2f8c87cb05189a767
MichaK726 <145672657+MichaK726@users.noreply.github.com>
2024-04-19 23:06:35 +0200

    Made a decent start on the rope (Not finished yet)
    I've made it so the rope is a collection of rigidbodies tied together with PinJoint3d's using a Rope3D plugin.

---
+++ Scripts/rope.gd
@@ -0,0 +1,22 @@
+extends Node3D
+
+const RopeScene: PackedScene = preload("res://addons/rope3d/Rope3D.tscn")
+
+@onready var start_point = $RootNode/RopeStartPoint
+@onready var end_point = $RootNode/RopeEndPoint
+
+
+func _ready():
+	var rope = RopeScene.instantiate()
+	rope.start_point = start_point
+	rope.end_point = end_point
+	rope.rope_length = 2.5
+	rope.resolution = 6.0
+	rope.radius = 0.1
+	
+	var IsValidRope = rope.can_make()
+	print(IsValidRope)
+	if IsValidRope:
+		add_child(rope)
+		rope.make()
+	
