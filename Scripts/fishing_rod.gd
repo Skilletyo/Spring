745556a2f5364622cdb53e8a1f524c28d8c9ba3f
MichaK726 <145672657+MichaK726@users.noreply.github.com>
2024-04-20 11:47:35 +0200

    Rope sorta works
    The rope is now better, but there is a lot of delay when the rope moves.

---
+++ Scripts/fishing_rod.gd
@@ -0,0 +1,10 @@
+extends Node3D
+
+var lure = preload("res://Prefabs/fishing_lure.tscn")
+
+func _process(delta):
+	if Input.is_action_pressed("ui_accept"):
+		var lureInstance = lure.instantiate()
+		lureInstance.position = $RootNode/LineStartPos.position
+		add_child(lureInstance)
+		lureInstance.apply_central_force(Vector3(20, 20, 20))
