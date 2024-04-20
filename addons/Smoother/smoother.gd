0cb4df2847208784ae63ca0208ac6fe234409d6e
proti <protizero@gmail.com>
2024-04-20 10:38:44 +0200

    Merging fishing rod and Level

--- addons/Smoother/smoother.gd
+++ addons/Smoother/smoother.gd
@@ -18,7 +18,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
-class_name Smoother extends Node
+class_name smoother extends Node
 
 ## Smoother Node
 ## Version: 1.0.4
